f258b6673cc3b7720156a0bb59fb77e7eba60a67
DankeZik <19938775+ArsDankeZik@users.noreply.github.com>
2021-11-10 00:21:35 +0100

    Cambiando el volumen general del googleTalk

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ const insultos = require('./insultos').insultos;
 // Variables para el programa
 var filepath = path.join(__dirname, 'prueba.wav');
 const SUBS = true; // CONSTANTE GLOBAL PARA HABILITAR CIERTOS COMANDOS SOLO PARA SUBS/VIPS/MODS
-const VOL = 0.2; // Controla el volumen de los sonidos !sonido
-const VERSION = '1.2.5';
+const VOL = 0.9; // Controla el volumen de los sonidos !sonido
+const VERSION = '1.2.6';
 var magicNumber = getRandInt(1, 50);
 var previousNumber = -1;
 var OBJECT_PEOPLE_LIFES = {};
